68bd541b612fdb3b7a1880af45974ba02a2e607d
SilverFix <silverfix@gmail.com>
2015-05-21 17:22:13 +0200

    fix celery worker

--- django_zilla/fabric_tasks/celery.py
+++ django_zilla/fabric_tasks/celery.py
@@ -3,18 +3,19 @@
 from __future__ import unicode_literals, division
 from fabric.decorators import task
 from fabric.operations import local
+from fabric.state import env
 
 
 @task
 def worker():
     """ Start celery worker """
-    print local('celery worker -A django_zilla.celery_manager -l INFO')
+    print local('celery worker -A %s -l INFO' % env.celery_app)
 
 
 @task
 def beat():
     """ Start celery beat """
-    print local('celery beat -A django_zilla.celery_manager -l INFO')
+    print local('celery beat -A %s -l INFO' % env.celery_app)
 
 
 @task
