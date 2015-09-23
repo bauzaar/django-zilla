cf223621408e7cf6b6ed70687d7f1c33d1f7f32f
Cristiano Valente <cristiano.valente@gmail.com>
2015-09-23 15:37:36 +0200

    Tasks to manage django's base commands.

--- django_zilla/fabric_tasks/app.py
+++ django_zilla/fabric_tasks/app.py
@@ -8,20 +8,18 @@ from django.conf import settings
 
 
 @task
-def prod():
-    """ --> [LOCAL] Compiles production frontend assets """
-    local('gulp prod')
+def run():
+    """ --> [LOCAL] Starts django development server """
+    local('python manage.py runserver')
 
 
 @task
-def fetch():
-    """ --> [LOCAL] Updates gulp-zilla's repository """
-    with lcd(settings.GULP_ZILLA_SRC_PATH):
-        local('git pull')
+def shell():
+    """ --> [LOCAL] Starts django shell """
+    local('python manage.py shell')
 
 
 @task
-def update():
-    """ --> [LOCAL] Installs or updates gulp-zilla's dependencies """
-    with lcd(settings.GULP_ZILLA_SRC_PATH):
-        local('gulp install')
+def dbshell():
+    """ --> [LOCAL] Launches db shell through django """
+    local('python manage.py dbshell')
